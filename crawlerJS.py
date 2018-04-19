0f6eb6b0b698a53e3e364a296cf89c5e29ed5b20
lucasgsfelix <lucasgsfelix@gmail.com>
2018-04-18 21:24:04 -0300

    Add files via upload

---
+++ crawlerJS.py
@@ -0,0 +1,48 @@
+#-*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import unicodedata
+import re
+import parser 
+### crawler para a página do reclame aqui para disciplina de mineração de dados
+
+
+
+
+if __name__ == "__main__":
+
+	idsEmpresas = ["4421", "1492", "7712", "2852"]
+	page =  1
+	#link = "https://www.reclameaqui.com.br/indices/lista_reclamacoes/?id="+idEmpresa+"&page="+page+"&size=10&status=ALL"
+	driver = webdriver.Chrome()
+	for idEmpresa in idsEmpresas:
+
+		while(1):
+
+			driver.get("https://www.reclameaqui.com.br/indices/lista_reclamacoes/?id="+str(idEmpresa)+"&page="+str(page)+"&size=10&status=ALL")
+			html = driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
+			links, nomeEmpresa = parser.retiraLinks(html)
+			#driver.close()
+	
+			for i in range(0, len(links)):
+				#https://www.reclameaqui.com.br/vivo-celular-fixo-internet-tv/vivo-sem-fibra_VLyoNXl_8gkiMgi8/
+				montaLink = "https://www.reclameaqui.com.br/"+nomeEmpresa+links[i]
+				driver.get(montaLink)
+				html = driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
+				parser.retiraInfo(html)
+				#driver.close()
+				exit()
+
+				
+
+
+			page = page + 1
+
+
+	arq = open("saida.txt", 'w')
+	html = html.encode('utf-8')
+	arq.write(html)
+	#print(p_element.text)
+
+
+#phantomjs save_page.js https://www.reclameaqui.com.br/empresa/oi-movel-fixo-tv/ > page.html
---
+++ parser.py
@@ -0,0 +1,86 @@
+#-*- coding: utf-8 -*-
+### parser para as páginas
+import re
+import unicodedata
+
+def retiraLinks(html):
+
+	r = [(a.end()) for a in list(re.finditer("href=\"/empresa/", html))]
+	p = r[0]
+	nomeEmpresa = [] #### essa parte vai descobrir o nome da empresa
+	while html[p] != "/":
+		nomeEmpresa.append(html[p])
+		p=p+1
+
+	###################### links para cada pergunta
+	r = [(a.end()) for a in list(re.finditer("href=\"/"+''.join(nomeEmpresa), html))]
+	html = list(html) ### transformando numa lista
+	links = []
+	for posicao in r:
+		p = posicao
+		link = []
+		while html[p] != "\"":
+			link.append(html[p])
+			p=p+1
+		links.append(''.join(link))
+
+	return links, ''.join(nomeEmpresa)
+
+def retiraInfo(html):
+
+
+	r = [(a.end()) for a in list(re.finditer("ID: ", html))] ### primeiro irei pegar o id
+	idReclamacao = parseIt(r, html, "<")
+	r = [(a.end()) for a in list(re.finditer("<ul class=\"local-date list-inline\"> <li class=\"ng-binding\"><img src=\"../../../images/pin-maps.52fa5ca3.png\" height=\"14\" width=\"10\">", html))] ###pegando o local <ul class="local-date list-inline"> <li class="ng-binding"><img src="../../../images/pin-maps.52fa5ca3.png" height="14" width="10">
+	local = parseIt(r, html, "<")
+	r = [(a.end()) for a in list(re.finditer("<li class=\"ng-binding\"><i class=\"fa fa-calendar\"></i>", html))] ### pega o horário e data <li class="ng-binding"><i class="fa fa-calendar"></i>
+	horario = parseIt(r, html, "<")
+	r = [(a.end()) for a in list(re.finditer("/busca/", html))] ### pegando tópicos associados
+	topicosAssociados = parseIt(r, html, "\"")
+	r = [(a.end()) for a in list(re.finditer("<p ng-bind-html=\"reading.complains.description|textModerateDecorator\" class=\"ng-binding\">", html))] #pegando a reclamação <p ng-bind-html="reading.complains.description|textModerateDecorator" class="ng-binding">
+	reclamacao = parseIt(r, html, "</p>")
+
+	#### O que foi dito pelo consumidor, o local onde ele está,  se foi respondida ou não, se foi qual a resposta
+	#### o que ele falou, data e hora de postagem, tópicos associados aquela postagem
+
+
+def parseIt(posicoes, html, final):
+
+	retornos = []
+	if final != "</p>":
+		for i in range(0, len(posicoes)):
+			p = posicoes[i]
+			ret = []
+			while html[p] != final:
+				ret.append(html[p])
+				p=p+1
+
+			retornos.append(''.join(ret))
+	else:
+		p = posicoes[0]
+		r = [(a.start()) for a in list(re.finditer("</p>", html))]
+
+		for k in r:
+			if k>p:
+				break
+
+		flag=0
+		while p<k:
+			if html[p]==">":
+				flag=1
+				p=p+1
+
+			if flag==1:
+				retornos.append(html[p])
+
+			p=p+1
+
+		retornos = ''.join(retornos)
+
+
+
+	return retornos
+
+arq = open("teste.txt", "r")
+html = arq.read()
+retiraInfo(html)
