17fcca7c5c853dc5ed13791b95393f717f649c31
lucasgsfelix <lucasgsfelix@gmail.com>
2018-06-09 19:40:01 -0300

    Add files via upload

--- crawlerJS.py
+++ crawlerJS.py
@@ -38,13 +38,14 @@ if __name__ == "__main__":
 	#idsEmpresas = ["4421", "1492", "7712", "2852"]
 	idsEmpresas = ['7712']
 	#link = "https://www.reclameaqui.com.br/indices/lista_reclamacoes/?id="+idEmpresa+"&page="+page+"&size=10&status=ALL"
-
+	quantidadeTransacoes = 10000
+	flag=0
 	driver = webdriver.Chrome()
 	for idEmpresa in idsEmpresas:
 
-		page = 1
-
-		while(page<1000):
+		qt = 0
+		### fazer flag dos 15 minutos
+		while(qt<quantidadeTransacoes):
 
 			links = []
 			while len(links) == 0:
@@ -62,10 +63,13 @@ if __name__ == "__main__":
 						driver.get(montaLink)
 						html = driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
 						parser.retiraInfo(html, nomeEmpresa)
+						flag=1
 					except:
 						print "Page Erro ! Link: " + montaLink
-				
 
+					if flag == 1:
+						qt=qt+1
+						flag=0
 				
 			page = page + 1
 
