038493306749dd7a194333cb23878162620e3550
Luiz Angioletti <luizfelipeasoares@gmail.com>
2021-01-12 17:14:02 -0300

    Formata PEP-8; fecha driver ao final; escreve arquivo de saida fora do parser.py (e garante consistência); adiciona o link como campo da saída do arquivo; processo mais pythonico como um todo; qt reduzido para fins de debug

--- crawlerJS.py
+++ crawlerJS.py
@@ -50,45 +50,63 @@ def retiraLinksProibidos(links):
 
 if __name__ == "__main__":
 
-    #idsEmpresas = ["4421", "1492", "7712", "2852"]
-    idsEmpresas = ['4421']
-    quantidadeTransacoes = 10000
-    flag=0
-    driver = webdriver.Chrome()
-    for idEmpresa in idsEmpresas:
+    # idsEmpresas = ["4421", "1492", "7712", "2852"]
+    Empresas = ['4421']
 
+    # link = "https://www.reclameaqui.com.br/indices/lista_reclamacoes/?id="
+    #        +idEmpresa
+    #        +"&page="
+    #        +page
+    #        +"&size=10&status=ALL"
+    output_file = 'reclamacoes.tsv'
+    file_header = ['Link','Id_Reclamacao','Nome_Empresa','Titulo_Reclamacao',
+                  'Local_Postagem', 'Horario_Postagem', 'Topicos_Associados',
+                  'Texto_Reclamacao','\n']
+    quantidadeTransacoes = 1
+    driver = webdriver.Chrome()
+
+    # testa se o arquivo já existe. Se existir, remove.
+    # Isso é necessário porque a cada vez que o arquivo for aberto daqui pra
+    # frente ele terá apenas conteúdo adicionado
+    if os.path.isfile(output_file):
+        os.remove(output_file)
+
+    # escreve cabeçalho no arquivo de saída
+    with open(output_file, 'w') as f:
+        f.write('\t'.join(file_header))
+
+    for idEmpresa in Empresas:
         qt = 0
         page = 1
         ### fazer flag dos 15 minutos
         while(qt<quantidadeTransacoes):
-
             links = []
             while len(links) == 0:
-
                 links, nomeEmpresa = pegaLinks(page, idEmpresa)
-                if links is None: break
-
+                if links is None:
+                    break
             if links is not None and nomeEmpresa is not None:
-                for i in range(0, len(links)):
-                    montaLink = "https://www.reclameaqui.com.br/"+nomeEmpresa+links[i]
-                    print(montaLink)
-                    try:
-                        inicio = time.time()
-                        driver.get(montaLink)
-                        final = time.time()
-                        if final - inicio >= 1:
-                                driver.refresh() ### para evitar erros onde não carrega a página
-                        html = driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
-                        parser.retiraInfo(html, nomeEmpresa)
-                        flag=1
-                    except:
-                        print("Page Erro ! Link: " + montaLink)
+                for item in links:
+                     montaLink = "https://www.reclameaqui.com.br/" \
+                                 +nomeEmpresa \
+                                 +item
+                     print(montaLink)
+                     try:
+                         inicio = time.time()
+                         driver.get(montaLink)
+                         final = time.time()
+                         if final - inicio >= 1:
+                             # para evitar erros onde não carrega a página
+                             driver.refresh()
+                             script = "return document.getElementsByTagName('html')[0].innerHTML"
+                             html = driver.execute_script(script)
+                         time.sleep(1)
+                         info = parser.retiraInfo(html, nomeEmpresa)
+                         with open(output_file, 'a') as f:
+                             f.write(montaLink + '\t' + info + '\n')
+                     except:
+                         print( "Page Erro ! Link: " + montaLink)
 
-                    if flag == 1:
-                        qt=qt+1
-                        flag=0
+            qt += 1
             page = page + 1
-
-    with open('saida.txt', 'w') as f:
-        html = html.encode('utf-8')
-        f.write(html)
+    driver.close()
