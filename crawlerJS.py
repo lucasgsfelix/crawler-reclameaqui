4106531aba810d51ab1160781031496a3c0b473b
Luiz Angioletti <73172465+angioletti@users.noreply.github.com>
2021-01-13 09:00:52 -0300

    Reformulação e organização geral de código (#1)
    * Adicionar arquivo .gitignore com arquivo temporário do VIM
    
    * Adicionar cabeçalho de código, alterar estilo para PEP-8
    
    * Formata pegaLinks para PEP8
    
    * Formata retiraLinksProibidos() para PEP8; altera a forma de abertura de arquivo para usar with()
    
    * Formata PEP-8; fecha driver ao final; escreve arquivo de saida fora do parser.py (e garante consistência); adiciona o link como campo da saída do arquivo; processo mais pythonico como um todo; qt reduzido para fins de debug
    
    * retiraLinks(): formata PEP-8, organiza, comenta o código, remove transformações em listas para objeto que já é iterável
    
    * :retiraRepetidos(): formata PEP-8
    
    * retiraInfo(): formata PEP-8; organiza e comenta o código; remove transformações de lista desnecessárias; remove função de escrita em arquivo de saida, que agora é feito pelo main do crawler
    
    * adiciona novo arquivo de saída no gitignore
    
    * strings quebradas de acordo com PEP-8

--- crawlerJS.py
+++ crawlerJS.py
@@ -3,100 +3,113 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unicodedata
 import re
-import parser 
+import parser
 import requests
 import time
-### crawler para a página do reclame aqui para disciplina de mineração de dados
+import os
+####################################################
+##
+## Crawler Reclame Aqui
+## Autor: Lucas G. Felix
+## Contribuição: Luiz Angioletti
+##
+####################################################
 
 def pegaLinks(page, idEmpresa):
+    try:
+        driver.get(
+            "https://www.reclameaqui.com.br/indices/lista_reclamacoes/?id="
+            +str(idEmpresa)
+            +"&page="
+            +str(page)
+            +"&size=10&status=ALL"
+        )
 
-	try:
-		driver.get("https://www.reclameaqui.com.br/indices/lista_reclamacoes/?id="+str(idEmpresa)+"&page="+str(page)+"&size=10&status=ALL")
-		html = driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
-		links, nomeEmpresa = parser.retiraLinks(html)
+        html = driver.execute_script(
+                "return document.getElementsByTagName('html')[0].innerHTML"
+               )
+        links, nomeEmpresa = parser.retiraLinks(html)
+        links = retiraLinksProibidos(links)
 
-		links = retiraLinksProibidos(links)
+        return links, nomeEmpresa
 
+    except:
+        return None, None
 
-		return links, nomeEmpresa
-
-	except:
-
-		return None, None
-
+# Revisar se está realmente retirando os links proibidos
 def retiraLinksProibidos(links):
+    '''Retira links que estão listados no arquivo de links proibidos'''
+    with open('Aux/links_proibidos') as f:
+        links_proibidos = f.read().split('\n')
 
-	arq = open("Aux/links_proibidos")
-	links_proibidos = arq.read()
-	links_proibidos = links_proibidos.split('\n')
-	arq.close()
+    if links is not None:
+        for count, item in enumerate(links):
+            if item in links_proibidos:
+                links.pop(count) ## retirando o link proibido
+    return links
 
-	if links is not None:
-		i=0
-		while i<len(links):
-			
-			j=0
-			while j<len(links_proibidos):
-
-				if links[i] == links_proibidos[j]:
-
-					links.pop(i) ## retirando o link proibido
-					i=i-1
-					break
-				
-				j=j+1
-
-			i=i+1
-
-	return links
-
-	
 if __name__ == "__main__":
 
-	#idsEmpresas = ["4421", "1492", "7712", "2852"]
-	idsEmpresas = ['2852']
-	#link = "https://www.reclameaqui.com.br/indices/lista_reclamacoes/?id="+idEmpresa+"&page="+page+"&size=10&status=ALL"
-	quantidadeTransacoes = 10000
-	flag=0
-	driver = webdriver.Chrome()
-	for idEmpresa in idsEmpresas:
+    # idsEmpresas = ["4421", "1492", "7712", "2852"]
+    Empresas = ['4421']
 
-		qt = 0
-		page = 1
-		### fazer flag dos 15 minutos
-		while(qt<quantidadeTransacoes):
+    # link = "https://www.reclameaqui.com.br/indices/lista_reclamacoes/?id="
+    #        +idEmpresa
+    #        +"&page="
+    #        +page
+    #        +"&size=10&status=ALL"
+    output_file = 'reclamacoes.tsv'
+    file_header = ['Link','Id_Reclamacao','Nome_Empresa','Titulo_Reclamacao',
+                  'Local_Postagem', 'Horario_Postagem', 'Topicos_Associados',
+                  'Texto_Reclamacao','\n']
+    quantidadeTransacoes = 1
+    driver = webdriver.Chrome()
 
-			links = []
-			while len(links) == 0:
+    # testa se o arquivo já existe. Se existir, remove.
+    # Isso é necessário porque a cada vez que o arquivo for aberto daqui pra
+    # frente ele terá apenas conteúdo adicionado
+    if os.path.isfile(output_file):
+        os.remove(output_file)
 
-				links, nomeEmpresa = pegaLinks(page, idEmpresa)
-				if links is None: break
+    # escreve cabeçalho no arquivo de saída
+    with open(output_file, 'w') as f:
+        f.write('\t'.join(file_header))
 
-			if links is not None and nomeEmpresa is not None:
-				
-				for i in range(0, len(links)):
-					
-					montaLink = "https://www.reclameaqui.com.br/"+nomeEmpresa+links[i]
-					print montaLink
-					try:
-						inicio = time.time()
-						driver.get(montaLink)
-						final = time.time()
-						if final - inicio >= 1:	
-							driver.refresh() ### para evitar erros onde não carrega a página
-						
-						html = driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
-						parser.retiraInfo(html, nomeEmpresa)
-						flag=1
-					except:
-						print "Page Erro ! Link: " + montaLink
+    for idEmpresa in Empresas:
+        qt = 0
+        page = 1
+        ### fazer flag dos 15 minutos
+        while(qt<quantidadeTransacoes):
+            links = []
+            while len(links) == 0:
+                links, nomeEmpresa = pegaLinks(page, idEmpresa)
+                if links is None:
+                    break
+            if links is not None and nomeEmpresa is not None:
+                for item in links:
+                     montaLink = "https://www.reclameaqui.com.br/" \
+                                 +nomeEmpresa \
+                                 +item
+                     print(montaLink)
+                     try:
+                         inicio = time.time()
+                         driver.get(montaLink)
+                         final = time.time()
+                         if final - inicio >= 1:
+                             # para evitar erros onde não carrega a página
+                             driver.refresh()
+                             # estratégia para quebrar strings muito longas é
+                             # colocar elas entre parênteses
+                             script = ("return document.getElements"
+                                       "ByTagName('html')[0].innerHTML")
+                             html = driver.execute_script(script)
+                         time.sleep(1)
+                         info = parser.retiraInfo(html, nomeEmpresa)
+                         with open(output_file, 'a') as f:
+                             f.write(montaLink + '\t' + info + '\n')
+                     except:
+                         print( "Page Erro ! Link: " + montaLink)
 
-					if flag == 1:
-						qt=qt+1
-						flag=0
-				
-			page = page + 1
-
-	arq = open("saida.txt", 'w')
-	html = html.encode('utf-8')
-	arq.write(html)
+            qt += 1
+            page = page + 1
+    driver.close()
--- parser.py
+++ parser.py
@@ -4,46 +4,50 @@ import re
 import unicodedata
 
 def retiraLinks(html):
+    try:
+        r = [a.end() for a in re.finditer("href=\"/empresa/", html)]
+        if len(r)>0:
+            # essa parte vai descobrir o nome da empresa:
+            # procura-se pela string dentro do finditer acima
+            # se ela for encontrada, armazenda todas as respostas.
+            # o finditer retorna posição de início e de fim da string buscada
+            # utilizamos apenas a de fim, porque queremos o que está após essa
+            # string buscada
+            p = r[0]
+            nomeEmpresa = []
+            # seguimos, abaixo, caracter a caracter até termos o nome
+            # completo da empresa em questão
+            while html[p] != "/":
+                    nomeEmpresa.append(html[p])
+                    p=p+1
 
-	r = [(a.end()) for a in list(re.finditer("href=\"/empresa/", html))]
-	if len(r)>0:
-		p = r[0]
-		nomeEmpresa = [] #### essa parte vai descobrir o nome da empresa
-		while html[p] != "/":
-			nomeEmpresa.append(html[p])
-			p=p+1
+            # ou não há nome para a empresa
+            if len(nomeEmpresa)==0:
+                    nomeEmpresa = "semnome"
 
-		if len(nomeEmpresa)==0:
-			nomeEmpresa = "semnome"
+            # usando a mesma lógica para encontrar os links para cada pergunta
+            r = [a.end() for a in re.finditer("href=\"/"+''.join(nomeEmpresa), html)]
+            links = []
+            for posicao in r:
+                p = posicao
+                link = []
+                while html[p] != "\"":
+                    link.append(html[p])
+                    p=p+1
+                links.append(''.join(link))
 
-		###################### links para cada pergunta
-		r = [(a.end()) for a in list(re.finditer("href=\"/"+''.join(nomeEmpresa), html))]
-		html = list(html) ### transformando numa lista
-		links = []
-		for posicao in r:
-			p = posicao
-			link = []
-			while html[p] != "\"":
-				link.append(html[p])
-				p=p+1
-			links.append(''.join(link))
-
-		return links, ''.join(nomeEmpresa)
-	
-	return None, None
+            return links, ''.join(nomeEmpresa)
+    except:
+        return None, None
 
 def retiraRepetidos(topicos):
-
-	if len(topicos)>1:
-		for i in range(1, len(topicos)):
-			if topicos[i] == topicos[i-1]:
-				topicos.pop(i)
-				if len(topicos)>=i:
-					break
-				
-
-
-	return topicos
+    if len(topicos)>1:
+        for i in range(1, len(topicos)):
+            if topicos[i] == topicos[i-1]:
+                topicos.pop(i)
+                if len(topicos)>=i:
+                    break
+    return topicos
 
 
 
@@ -73,48 +77,71 @@ def trataTopicosAssociados(topicosAssociados):
 	return t
 
 def retiraInfo(html, nomeEmpresa):
+    # pegando o título com o trecho:
+    # <div class="col-md-10 col-sm-12"> <h1 class="ng-binding">
+    r = [a.end() for a in re.finditer(
+               "<div class=\"col-md-10 col-sm-12\"> <h1 class=\"ng-binding\">",
+               html)]
 
+    titulo = parseIt(r, html, "<")
+    # print(titulo)
 
-	r  =[(a.end()) for a in list(re.finditer("<div class=\"col-md-10 col-sm-12\"> <h1 class=\"ng-binding\">", html))] #<div class="col-md-10 col-sm-12"> <h1 class="ng-binding">
-	titulo = parseIt(r, html, "<")
-	r = [(a.end()) for a in list(re.finditer("ID: ", html))] ### primeiro irei pegar o id
-	idReclamacao = parseIt(r, html, "<")
-	r = [(a.end()) for a in list(re.finditer("<ul class=\"local-date list-inline\"> <li class=\"ng-binding\"><img src=\"../../../images/pin-maps.52fa5ca3.png\" height=\"14\" width=\"10\">", html))] ###pegando o local <ul class="local-date list-inline"> <li class="ng-binding"><img src="../../../images/pin-maps.52fa5ca3.png" height="14" width="10">
-	local = parseIt(r, html, "<")
-	r = [(a.end()) for a in list(re.finditer("<li class=\"ng-binding\"><i class=\"fa fa-calendar\"></i>", html))] ### pega o horário e data <li class="ng-binding"><i class="fa fa-calendar"></i>
-	horario = parseIt(r, html, "<")
-	r = [(a.end()) for a in list(re.finditer("/busca/", html))] ### pegando tópicos associados
-	topicosAssociados = parseIt(r, html, "\"")
-	r = [(a.end()) for a in list(re.finditer("<p ng-bind-html=\"reading.complains.description|textModerateDecorator\" class=\"ng-binding\">", html))] #pegando a reclamação <p ng-bind-html="reading.complains.description|textModerateDecorator" class="ng-binding">
-	if len(r)==0:
-		print "Erro não há dados dá reclamação !"
-		return 
-	reclamacao = parseIt(r, html, "</p>")
+    # primeiro pegamos o ID
+    r = [a.end() for a in re.finditer("ID: ", html)]
+    idReclamacao = parseIt(r, html, "<")
+    # print(idReclamacao)
 
+    # pegando o local <ul class="local-date list-inline"> \
+    #                 <li class="ng-binding">\
+    #                 <img src="../../../images/pin-maps.52fa5ca3.png" \
+    #                 height="14" width="10">
+    var = "<ul class=\"local-date list-inline\"> <li class=\"ng-binding\"><img src=\"../../../images/pin-maps.52fa5ca3.png\" height=\"14\" \width=\"10\">"
+    r = [a.end() for a in re.finditer(var, html)]
+    local = parseIt(r, html, "<")
+    # print(local)
 
+    # pega o horário e data <li class="ng-binding">\
+    #                       <i class="fa fa-calendar"></i>
 
-	reclamacao = reclamacao.replace("\n", " ")
-	reclamacao = reclamacao.replace('\t', " ")
+    var = "<li class=\"ng-binding\"><i class=\"fa fa-calendar\"></i>"
+    r = [a.end() for a in re.finditer(var, html)]
+    horario = parseIt(r, html, "<")
+    # print(horario)
 
+    # pegando tópicos associados
+    r = [a.end() for a in re.finditer("/busca/", html)]
+    topicosAssociados = parseIt(r, html, "\"")
+    # print(topicosAssociados)
 
-	if topicosAssociados is not None:
-		t = trataTopicosAssociados(topicosAssociados)
-	else:
-		t = "-"
-	
+    # pegando a reclamação <p ng-bind-html="reading.complains.\
+    #                      description|textModerateDecorator" \
+    #                      class="ng-binding">
+    var = "<p ng-bind-html=\"reading.complains.description|textModerateDecorator\" class=\"ng-binding\">"
 
-	arq = open("tim.txt", "a")
+    r = [a.end() for a in re.finditer(var, html)]
+    if len(r)==0:
+        print("Erro não há dados da reclamação !")
+        return
 
-	info = idReclamacao[0]+'\t'+nomeEmpresa+'\t'+titulo[0]+'\t'+local[0]+'\t'+horario[0]+'\t'+t+'\t'+reclamacao+'\n'
-	arq.write(u''.join(info).encode('utf-8'))
-	arq.close()
-			
-	
-		
+    reclamacao = parseIt(r, html, "</p>")
+    reclamacao = reclamacao.replace("\n", " ")
+    reclamacao = reclamacao.replace('\t', " ")
 
-	#### O que foi dito pelo consumidor, o local onde ele está,  se foi respondida ou não, se foi qual a resposta
-	#### o que ele falou, data e hora de postagem, tópicos associados aquela postagem
+    # print(reclamacao)
 
+    if topicosAssociados is not None:
+            t = trataTopicosAssociados(topicosAssociados)
+    else:
+            t = "-"
+
+    # O id da reclamação, o nome da empresa, o título da postagem, 
+    # o local onde ele está, o horário em que o post foi feito,
+    # tópicos associadso à postagem (se existirem) e a reclamação de fato
+    info = idReclamacao[0] + '\t' + nomeEmpresa + '\t' + titulo[0] + \
+           '\t' + local[0] + '\t' + horario[0] + '\t' + t + \
+           '\t' + reclamacao
+
+    return info
 
 def parseIt(posicoes, html, final):
 
